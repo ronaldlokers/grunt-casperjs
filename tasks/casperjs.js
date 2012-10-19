ff72a310302d6771a2fcbb017dab25c5966bb048
Taku Fukushima <f.tac.mac@gmail.com>
2012-10-19 16:17:31 +0900

    Fixed the indentaions and elimitated the unnecessary spaces.
    Signed-off-by: Taku Fukushima <f.tac.mac@gmail.com>

--- tasks/casperjs.js
+++ tasks/casperjs.js
@@ -1,45 +1,40 @@
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
+  // Create a new multi task.
+  grunt.registerMultiTask('casperjs', 'This triggers casperjs.', function() {
+    // Tell grunt this task is asynchronous.
+    var done = this.async(),
+        files = grunt.file.expandFiles(this.file.src),
+        filepaths = [];
 
-    // Create a new multi task.
-    grunt.registerMultiTask( 'casperjs', 'This triggers casperjs.', function() {
-
-        // Tell grunt this task is asynchronous.
-        var done = this.async(),
-            files = grunt.file.expandFiles(this.file.src),
-            filepaths = [];
-
-        grunt.file.expandFiles(this.file.src).forEach(function(filepath) {
-            filepaths.push(filepath);
-        });
-
-        grunt.utils.async.forEachSeries(filepaths, function runCasper(filepath, callback) {
-            grunt.helper('casperjs', filepath, function(err){
-                if (err) {
-                    grunt.warn(err);
-                }
-                callback();
-            });
-        }, done);
-
+    grunt.file.expandFiles(this.file.src).forEach(function(filepath) {
+      filepaths.push(filepath);
     });
 
-    grunt.registerHelper('casperjs', function(filepath, callback) {
-        var command = 'casperjs "' + filepath + '"',
-            exec = require('child_process').exec;
+   grunt.utils.async.forEachSeries(filepaths, function(filepath, callback) {
+     grunt.helper('casperjs', filepath, function(err) {
+       if (err) {
+         grunt.warn(err);
+       }
+       callback();
+     });
+   }, done);
+ });
 
-        function puts( error, stdout, stderr ) {
+ grunt.registerHelper('casperjs', function(filepath, callback) {
+   var command = 'casperjs "' + filepath + '"',
+       exec = require('child_process').exec;
 
-            grunt.log.write( '\nRunning tests from "' + filepath + '":\n' );
-            grunt.log.write( stdout );
-            //grunt.log.error( stderr );
+   function puts(error, stdout, stderr) {
+     grunt.log.write('\nRunning tests from "' + filepath + '":\n');
+     grunt.log.write(stdout);
+     //grunt.log.error( stderr );
 
-            if ( error !== null ) {
-                callback(error);
-            } else {
-                callback();
-            }
-        }
-
-        exec( command, puts );
-    });
+     if ( error !== null ) {
+       callback(error);
+     } else {
+       callback();
+     }
+   }
+   exec(command, puts);
+ });
 };
