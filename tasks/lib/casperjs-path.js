3e57253ade8ef0f579244b7904f7859a4e065fe8
Jatin Chopra <jatin-@users.noreply.github.com>
2013-10-24 17:12:20 -0700

    Updated symbolic linking logic of existing casperjs installs.
     Resolves ronaldlokers/grunt-casperjs#30

---
+++ tasks/lib/casperjs-path.js
@@ -0,0 +1,11 @@
+// Simple casperjs script to echo the casperjs path.
+var casper = require('casper').create();
+
+casper.start();
+
+casper.then(function getVersion() {
+    // phantom is a global variable created by casperjs.
+    this.echo(phantom.casperPath);
+});
+
+casper.run();
